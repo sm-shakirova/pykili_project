8edef4789b844b977f01bbd30e34ad3782e5b11c
Sofia <71773267+sm-shakirova@users.noreply.github.com>
2021-05-05 00:13:36 +0300

    Add files via upload

---
+++ code/parser.py
@@ -0,0 +1,23 @@
+from pymorphy2 import MorphAnalyzer
+from string import punctuation
+import json
+
+
+def parse_text(data):
+    text = ' '.join(data)
+    text = [word.lower().strip(punctuation) for word in text.split()]
+    text = [word for word in text if word != '']
+    result = []
+    morph = MorphAnalyzer()
+    for word in text:
+        parser = morph.parse(word)[0]
+        dct = {'word': str(parser.word), 'lemma': str(parser.normal_form), 'tag': str(parser.tag)}
+        result.append(dct)
+    return result
+
+
+if __name__ == "__main__":
+    with open('example.json', 'r', encoding='utf-8') as file:
+        messages = json.load(file)
+    with open(input('Назвать новый файл: '), 'w', encoding='utf-8') as new_file:
+        json.dump(parse_text(messages), new_file, ensure_ascii=False, indent=2)
