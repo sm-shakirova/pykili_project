474ac170cc73124ddf26b614d06324ccf373b619
Sofia <71773267+sm-shakirova@users.noreply.github.com>
2021-05-08 13:25:25 +0300

    Update parser.py

--- code/parser.py
+++ code/parser.py
@@ -4,31 +4,29 @@ import json
 
 
 def parse_text(data):
-    if isinstance(data, list):
-        data = ' '.join(data)
-    text = [word.lower().strip(punctuation) for word in data.split()]
-    text = [word for word in text if word != '']
     result = []
-    morph = MorphAnalyzer()  # NB! большой файл может грузить долго
-    for word in text:
-        parser = morph.parse(word)[0]
-        dct = {
-                  'word': str(parser.word),
-                  'lemma': str(parser.normal_form),
-                  'POS': str(parser.tag.POS),
-        }
-        tags = str(parser.tag).replace(f'{str(parser.tag.POS)},', '')
-        if tags == str(parser.tag.POS):
-            dct['tags'] = None
-        elif tags != '':
-            dct['tags'] = tags
-        # список тэгов: https://pymorphy2.readthedocs.io/en/latest/user/grammemes.html
-        result.append(dct)
+    morph = MorphAnalyzer()  # NB! долго парсит большие файлы 
+    for el in data:
+        text = [word.lower().strip(punctuation) for word in el[1].split()]
+        text = [word for word in text if word != '']
+        for word in text:
+            parser = morph.parse(word)[0]
+            dct = {
+                    'year': el[0],
+                    'word': str(parser.word),
+                    'lemma': str(parser.normal_form),
+                    'POS': str(parser.tag.POS),
+                    'case': str(parser.tag.case),
+                    'number': str(parser.tag.number),
+                    'gender': str(parser.tag.gender)
+            }
+            # список тэгов: https://pymorphy2.readthedocs.io/en/latest/user/grammemes.html
+            result.append(dct)
     return result
 
 
 if __name__ == "__main__":
-    with open(input('Путь к файлу с текстом: '), 'r', encoding='utf-8') as file:
+    with open(input('Путь к файлу с вычлененными текстами: '), 'r', encoding='utf-8') as file:
         messages = json.load(file)
     with open(input('Назвать новый файл: '), 'w', encoding='utf-8') as new_file:
-        json.dump(parse_text(messages), new_file, ensure_ascii=False, indent=2)
+        json.dump(parse_text(messages), new_file, ensure_ascii=False, indent=1)
