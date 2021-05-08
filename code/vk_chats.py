d79d2cbe6df68062168e4e8f308c763dff2b59ef
Sofia <71773267+sm-shakirova@users.noreply.github.com>
2021-05-08 13:23:26 +0300

    Update vk_chats.py

--- code/vk_chats.py
+++ code/vk_chats.py
@@ -10,21 +10,23 @@ def get_messages(file_path):
         data = data.replace(smile, '')
     for link in set(re.findall(r'http\S+', data)):  # удаляет ссылки
         data = data.replace(link, '')
-    messages = re.findall(r'(?<=<div>).+?(?=<div class="kludges">)', data)  # находит тексты сообщений
+    messages = re.findall(r'(?<=<div class="message__header">).+?\n.+?(?=<div class="kludges">)', data)
     for ind, message in enumerate(messages):
-        messages[ind] = message.replace('<br>', '').replace('&quot;', '')
+        year = re.search(r'\d{4}', message)[0]  # находит год
+        message = re.search(r'(?<=<div>).*', message)[0]  # вычленяет текст сообщения
+        message = message.replace('<br>', '').replace('&quot;', '')
+        if message != '':
+            messages[ind] = [year, message]
     return messages
 
 
 if __name__ == "__main__":
-    path = input('Путь к папке с сообщениями из VK на вашем компьютере: ')
-    all_texts = []
+    path = input('Путь к папке messages: ')
+    text = []
     for dirs, folder, files in os.walk(path):
-        # рекурсивная штука, поэтому находит все файлы, в том числе во внутренних папках
-        # документация: https://docs.python.org/3/library/os.html#os.walk
-        # еще есть listdir, но он проходится только по поверхности
         for file in files:
-            texts = get_messages(os.path.join(dirs, file))
-            all_texts.extend(texts)
+            messages = get_messages(os.path.join(dirs, file))
+            text.extend(messages)
     with open(input('Назвать новый файл: '), 'w', encoding='utf-8') as new_file:
-        json.dump(all_texts, new_file, ensure_ascii=False, indent=2)
+        json.dump(text, new_file, ensure_ascii=False, indent=2)
+        
