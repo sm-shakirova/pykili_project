2bb307e928e0048c1a31af7adb1fb46b8c904cff
Sofia <71773267+sm-shakirova@users.noreply.github.com>
2021-05-11 13:57:39 +0300

    Update parser.py

--- code/parser.py
+++ code/parser.py
@@ -4,15 +4,33 @@ import json
 
 
 def parse_text(data):
+    """Функция принимает список текстов с годами их написания и делает разбор каждого слова"""
     result = []
-    morph = MorphAnalyzer()  # NB! долго парсит большие файлы 
-    for el in data:
-        text = [word.lower().strip(punctuation) for word in el[1].split()]
+    morph = MorphAnalyzer()
+    if isinstance(data[0], list):
+        for el in data:
+            text = [word.lower().strip(punctuation) for word in el[1].split()]
+            text = [word for word in text if word != '']
+            for word in text:
+                parser = morph.parse(word)[0]
+                dct = {
+                        'year': el[0],
+                        'word': str(parser.word),
+                        'lemma': str(parser.normal_form),
+                        'POS': str(parser.tag.POS),
+                        'case': str(parser.tag.case),
+                        'number': str(parser.tag.number),
+                        'gender': str(parser.tag.gender)
+                }
+                # список тэгов: https://pymorphy2.readthedocs.io/en/latest/user/grammemes.html
+                result.append(dct)
+    else:
+        text = [word.lower().strip(punctuation) for word in data[1].split()]
         text = [word for word in text if word != '']
         for word in text:
             parser = morph.parse(word)[0]
             dct = {
-                    'year': el[0],
+                    'year': data[0],
                     'word': str(parser.word),
                     'lemma': str(parser.normal_form),
                     'POS': str(parser.tag.POS),
@@ -20,13 +38,12 @@ def parse_text(data):
                     'number': str(parser.tag.number),
                     'gender': str(parser.tag.gender)
             }
-            # список тэгов: https://pymorphy2.readthedocs.io/en/latest/user/grammemes.html
             result.append(dct)
     return result
 
 
 if __name__ == "__main__":
-    with open(input('Путь к файлу с вычлененными текстами: '), 'r', encoding='utf-8') as file:
+    with open(input('Путь к файлу с текстами (и годами): '), 'r', encoding='utf-8') as file:
         messages = json.load(file)
     with open(input('Назвать новый файл: '), 'w', encoding='utf-8') as new_file:
         json.dump(parse_text(messages), new_file, ensure_ascii=False, indent=1)
