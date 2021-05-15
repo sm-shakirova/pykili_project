f799beaf3362052ac466985d5830edbc6d89c8b5
Sofia <71773267+sm-shakirova@users.noreply.github.com>
2021-05-15 04:54:24 +0300

    Update research_1.py

--- code/research_1.py
+++ code/research_1.py
@@ -4,14 +4,14 @@ import json
 def extract_adjectives(lst):
     """Функция для сортировки сочетаний прилагательных с существительными по годам"""
     noun_indexes = []
-    for ind, word in enumerate(lst):
+    for ind, word in enumerate(lst):  
         if word['POS'] == 'NOUN':
-            noun_indexes.append(ind)
+            noun_indexes.append(ind)  # записывает индексы существительных
     dct = {}
     for ind, word in enumerate(lst):
-        if word['POS'] == 'ADJF':
+        if word['POS'] == 'ADJF':  # находит прилагательные
             noun = False
-            for noun_ind in noun_indexes:
+            for noun_ind in noun_indexes:  # сверяет индексы прилагательных с индексами существительных
                 if noun_ind - ind == 1:
                     noun = lst[noun_ind]
                 elif noun_ind - ind == 2:
@@ -20,7 +20,7 @@ def extract_adjectives(lst):
                     and word['year'] == noun['year'] \
                     and word['case'] == noun['case'] \
                     and word['number'] == noun['number'] \
-                    and word['gender'] == noun['gender']:
+                    and word['gender'] == noun['gender']:  # проверяет согласование существительного и прилагательного
                 if word['year'] in dct:
                     if noun['lemma'] in dct[word['year']]:
                         dct[word['year']][noun['lemma']].append(word['lemma'])
@@ -37,8 +37,8 @@ def sort_adjectives(dct):
         for noun, inner_list in inner_dict.items():
             adj_dict = dict.fromkeys(set(inner_list))
             for adj in adj_dict:
-                adj_dict[adj] = inner_list.count(adj)
-            adj_dict = sorted(adj_dict, key=adj_dict.get, reverse=True)
+                adj_dict[adj] = inner_list.count(adj)  # считает количество одинаковых прилагательных для каждого существительного
+            adj_dict = sorted(adj_dict, key=adj_dict.get, reverse=True)  # сортирует по количеству повторений
             inner_dict[noun] = [key for key in adj_dict]
         dct[year] = inner_dict
     return dct
